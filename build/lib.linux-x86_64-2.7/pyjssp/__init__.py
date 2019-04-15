0596f8fe81e6210b8d8a3079fd7e5d822b806092
junyoung.park <junyoungpark@kaist.ac.kr>
2019-04-15 11:07:36 +0900

    vesion up! support benchmarks

---
+++ build/lib.linux-x86_64-2.7/pyjssp/__init__.py
@@ -0,0 +1,4 @@
+from .utils import *
+from .jobShopSamplers import *
+from .machineHelpers import *
+from .simulators import *
---
+++ build/lib.linux-x86_64-2.7/pyjssp/configs.py
@@ -0,0 +1,19 @@
+# Simulator node types
+NOT_START_NODE_SIG = -1
+PROCESSING_NODE_SIG = 0
+DONE_NODE_SIG = 1
+DELAYED_NODE_SIG = 2
+DUMMY_NODE_SIG = 3
+
+# Simulator edge types
+CONJUNCTIVE_TYPE = 0
+DISJUNCTIVE_TYPE = 1
+
+# Simulator edge directions
+FORWARD = 0
+BACKWARD = 1
+
+# miscellaneous
+N_SEP = 1
+SEP = ' '
+NEW = '\n'
